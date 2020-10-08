3187b8f5bbee244fdb41b7d2b7ede3945c4f979c
Lilian Nicolau <lianicolau30@gmail.com>
2020-10-08 15:50:07 -0300

    Exercício de objetos - entrega final

--- semana4/objetos/blog-boilerplate/index.js
+++ semana4/objetos/blog-boilerplate/index.js
@@ -1,23 +1,36 @@
 let blog = () => {
-  let armazenaTitulo = document.getElementById("titulo-post");
-  let armazenaAutor = document.getElementById("autor-post");
-  let armazenaConteudo = document.getElementById("conteudo-post");
+  let armazenaTitulo = document.getElementById("titulo-post"); //variável de pega título do post  na página html
+  let armazenaAutor = document.getElementById("autor-post");// variável que pega nome do autor digitado na página
+  let armazenaConteudo = document.getElementById("conteudo-post"); // variável que pega o conteúdo digitado na página
 
-  const post = {
-    titulo: armazenaTitulo.value,
-    autor: armazenaAutor.value,
-    conteudo: armazenaConteudo.value,
+  let post = { // criando um objeto
+    titulo: armazenaTitulo.value, //chave que pega o valor da varável armazenaTitulo
+    autor: armazenaAutor.value, //chave que pega o valor da variável armazenaAutor 
+    conteudo: armazenaConteudo.value, //chave que pega o valor da varável armazenaConteudo
   };
+
+  let localImpressao = document.getElementById("container-de-posts") //local para imprimir os posts
+  localImpressao.innerHTML += `<p>${post.titulo}, ${post.autor}, ${post.conteudo},</p>`//vai imprimir o conteódo do objeto
 
   console.log(post);
 };
 
-const apertouBotao = () => {
+const apertouBotao = () => { //função que deixa o valor ser inserido clicando no botão
   blog();
 };
 
-const apertouEnter = (event) => {
+const apertouEnter = (event) => { //função que deixa o valor ser inserido apertando enter
   if (event.key === "Enter") {
     blog();
   }
 };
+
+/*let apagaForm = () => {
+	document.getElementById("titulo-post").value = "";
+	document.getElementById("autor.post").value = "";
+	document.getElementById("conteudo.post").value = "";
+}
+
+console.log(apagaForm)*/
+
+
