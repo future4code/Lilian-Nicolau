50ca0d794440609edc50ceefa4bb71c8bcf95377
Lilian Nicolau <lianicolau30@gmail.com>
2020-10-20 22:18:54 -0300

    labenu-forms

---
+++ semana6/labenu-forms/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import style from "styled-components";
+import Form1 from './components/Form1';
+import Form2 from './components/Form2';
+import Form3 from './components/Form3';
+import ThankUpage from './components/ThankUpage';
+
+
+export default class App extends React.Component {
+  state = {
+
+  }
+  render() {
+
+  
+  return (
+    <div className="App">
+      
+      
+    </div>
+  );
+}
+}
+export default App;
---
+++ semana6/labenu-forms/src/components/Form2.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function Form2() {
+  return (
+    <div className="Form2">
+      
+    </div>
+  );
+}
+
+export default Form2;
